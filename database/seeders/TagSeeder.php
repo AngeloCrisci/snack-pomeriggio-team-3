ea2b8335f97a0f4fa2ee2849eed7fffc974c87e6
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-11 17:36:11 +0100

    added faker to seeder tags

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -4,13 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TagSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //
     }
