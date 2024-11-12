c5af8e8fa3fe5b2a477f78149e05c6cfd3050add
orsoli <fljcoli@gmail.com>
2024-11-12 16:19:22 +0100

    Create CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->prefix('admin/')->group(function(){
+    Route::resource('posts', PostController::class);
+    Route::get('posts/trash', [PostController::class, 'trashed'])->name('trash');
+});
