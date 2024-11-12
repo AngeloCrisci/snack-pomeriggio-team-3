51459ab843c497658fb58992f2a869a57ff89343
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-12 16:13:43 +0100

    show complete

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -36,7 +37,8 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        return view("admin.posts.show", compact("post"));
     }
 
     /**
