52b7316a9ccb2d9e35e68a58e6296408a6a69722
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-12 16:39:05 +0100

    tags relation

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+    // aggiungi le fillables
+    protected $fillable = [
+        'name',
+        'color',
+    ];
+
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -13,6 +14,22 @@ class TagSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        //
+        $tagsNames = [
+            'Sad',
+            'Hilarious',
+            'Dramatic',
+            'News',
+            'Historic',
+            'LongRead',
+            'QuickRead',
+        ];
+        foreach($tagsNames as $tagName){
+            $newTag = new Tag();
+            $newTag->name = $tagName;
+            $newTag->color = $faker->unique()->hexColor();
+            $newTag->save();
+        }
+
+
     }
 }
